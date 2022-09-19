b269f651ee3bf244af8a0d49686d6359c2f41493
unknown <manupashu@gmail.com>
2022-09-19 11:59:31 +0530

    modified pom and serverport

--- src/main/java/com/start/rest/RestController.java
+++ src/main/java/com/start/rest/RestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 	
-	@GetMapping
+	@GetMapping("/greet")
 	public String Welcome()
 	{
 		return "Welcome to Boot";
