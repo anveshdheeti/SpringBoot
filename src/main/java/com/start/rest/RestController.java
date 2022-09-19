00be42ab314a9243ecb813615c3befbb2865ad83
unknown <manupashu@gmail.com>
2022-09-19 20:23:48 +0530

    added mapping url

--- src/main/java/com/start/rest/RestController.java
+++ src/main/java/com/start/rest/RestController.java
@@ -1,8 +1,10 @@
 package com.start.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/Greetings")
 public class RestController {
 	
 	@GetMapping("/greet")
